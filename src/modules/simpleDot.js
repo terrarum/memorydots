2f2f7b4196ec9708490d3f3738838000554b4df0
James Fidler <terrarum@gmail.com>
2020-07-27 18:16:54 +0100

    removed separate module to make memory usage difference more drastic

--- src/modules/simpleDot.js
+++ src/modules/simpleDot.js
@@ -1,11 +1,22 @@
 import random from 'lodash.random';
-import updatePosition from './updatePosition';
 
 const SimpleDot = function SimpleDot(ctx) {
   this.x = random(0, ctx.canvas.width);
   this.y = random(0, ctx.canvas.height);
 
-  this.updatePosition = updatePosition;
+  this.updatePosition = function updatePosition() {
+    let newX = this.x + random(-1, 1);
+    let newY = this.y + random(-1, 1);
+
+    newX = newX < -5 ? ctx.canvas.width + 5 : newX;
+    newX = newX > ctx.canvas.width + 5 ? -5 : newX;
+
+    newY = newY < -5 ? ctx.canvas.height + 5 : newY;
+    newY = newY > ctx.canvas.height + 5 ? -5 : newY;
+
+    this.x = newX;
+    this.y = newY;
+  };
 };
 
 export default SimpleDot;
