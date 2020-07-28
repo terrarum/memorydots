a0e92f26c1f622e961852fb6c80484128ed20e97
James Fidler <terrarum@gmail.com>
2020-07-28 16:02:25 +0100

    gutter var

--- src/modules/classDot.js
+++ src/modules/classDot.js
@@ -2,14 +2,16 @@ import random from 'lodash.random';
 
 class Dot {
   updatePosition(ctx) {
+    const gutterSize = 5;
+
     let newX = this.x + random(-1, 1);
     let newY = this.y + random(-1, 1);
 
-    newX = newX < -5 ? ctx.canvas.width + 5 : newX;
-    newX = newX > ctx.canvas.width + 5 ? -5 : newX;
+    newX = newX < -gutterSize ? ctx.canvas.width + gutterSize : newX;
+    newX = newX > ctx.canvas.width + gutterSize ? -gutterSize : newX;
 
-    newY = newY < -5 ? ctx.canvas.height + 5 : newY;
-    newY = newY > ctx.canvas.height + 5 ? -5 : newY;
+    newY = newY < -gutterSize ? ctx.canvas.height + gutterSize : newY;
+    newY = newY > ctx.canvas.height + gutterSize ? -gutterSize : newY;
 
     this.x = newX;
     this.y = newY;
--- src/modules/inheritDot.js
+++ src/modules/inheritDot.js
@@ -6,14 +6,16 @@ const InheritDot = function InheritDot(ctx) {
 };
 
 InheritDot.prototype.updatePosition = function updatePosition(ctx) {
+  const gutterSize = 5;
+
   let newX = this.x + random(-1, 1);
   let newY = this.y + random(-1, 1);
 
-  newX = newX < -5 ? ctx.canvas.width + 5 : newX;
-  newX = newX > ctx.canvas.width + 5 ? -5 : newX;
+  newX = newX < -gutterSize ? ctx.canvas.width + gutterSize : newX;
+  newX = newX > ctx.canvas.width + gutterSize ? -gutterSize : newX;
 
-  newY = newY < -5 ? ctx.canvas.height + 5 : newY;
-  newY = newY > ctx.canvas.height + 5 ? -5 : newY;
+  newY = newY < -gutterSize ? ctx.canvas.height + gutterSize : newY;
+  newY = newY > ctx.canvas.height + gutterSize ? -gutterSize : newY;
 
   this.x = newX;
   this.y = newY;
